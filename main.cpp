cab3cfd5fd51b0a67686e4b4fb123b97df01230d
Lucas Farias <lukf95@gmail.com>
2024-02-05 23:20:53 +0100

    update

--- main.cpp
+++ main.cpp
@@ -1,28 +1,47 @@
-#include <iostream>
 #include <boost/asio.hpp>
+#include <iostream>
+#include <vector>
+#include <atomic>
 
 using boost::asio::ip::tcp;
+using namespace std;
+
+atomic<int> client_counter(0);
+
+void handle_client(tcp::socket socket) {
+    int client_id = ++client_counter;
+    try {
+        cout << "Client " << client_id << " connected." << endl;
+        vector<char> data(1024);
+        while (true) {
+            boost::system::error_code error;
+            size_t length = socket.read_some(boost::asio::buffer(data), error);
+            if (error == boost::asio::error::eof) break;
+            else if (error) throw boost::system::system_error(error);
+
+            cout << "Client " << client_id << ": ";
+            cout.write(data.data(), length);
+            cout << endl;
+        }
+    } catch (std::exception &e) {
+        cerr << "Exception in client " << client_id << ": " << e.what() << endl;
+    }
+}
 
 int main() {
     try {
         boost::asio::io_context io_context;
-        tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 7666));
-        int client_count = 0;
 
-        std::cout << "Server running on port 7666. Waiting for connections..." << std::endl;
+        tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 1234));
 
         while (true) {
             tcp::socket socket(io_context);
             acceptor.accept(socket);
 
-            client_count++;
-            std::cout << "Client " << client_count << " connected." << std::endl;
-
-            std::string message = "Welcome, client " + std::to_string(client_count) + "!\n";
-            boost::asio::write(socket, boost::asio::buffer(message));
+            std::thread(handle_client, std::move(socket)).detach();
         }
     } catch (std::exception &e) {
-        std::cerr << e.what() << std::endl;
+        cerr << "Exception: " << e.what() << endl;
     }
 
     return 0;
